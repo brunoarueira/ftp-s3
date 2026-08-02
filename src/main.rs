8722b7310e63636b018995a3d7f1cc6579955538
Bruno Arueira <contato@brunoarueira.com>
2026-08-02 15:18:12 -0300

    refactor: changed from async_ftp to suppaftp

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,35 @@
-#[macro_use]
-extern crate lazy_static;
-
 mod s3;
 
 use std::error::Error;
-use async_ftp::FtpStream;
-use tokio::runtime::Builder;
+use suppaftp::tokio::AsyncRustlsFtpStream;
 
 use s3::save_to_s3;
 
-async fn async_main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     // Create a connection to an FTP server and authenticate to it.
-    let mut ftp_stream = FtpStream::connect("localhost:2100").await?;
-    let _ = ftp_stream.login("files", "files").await?;
+    let mut ftp_stream = AsyncRustlsFtpStream::connect("localhost:2100").await?;
+
+    ftp_stream.login("files", "files").await?;
 
     let entries = ftp_stream.nlst(None).await?;
 
-    for entry in entries.iter() {
-        match ftp_stream.get(&entry).await {
-            Ok(streamed_entry) => {
-                match save_to_s3(entry, streamed_entry).await {
-                    Ok(_) => {
-                        // feat flag option to remove file from ftp source after save on s3 successfully
-                        println!("entry {} is saved to s3", entry);
-                    },
-                    _ => { eprintln!("entry {} could not be saved to s3", entry) }
-                }
-            },
-            _ => { eprintln!("could not get {}", entry); }
+    for entry in entries {
+        let data_stream = ftp_stream.retr_as_stream(entry.clone()).await?;
+
+        match save_to_s3(&entry.clone(), data_stream).await {
+            Ok(_) => {
+                // feat flag option to remove file from ftp source after save on s3 successfully
+                println!("entry {} is saved to s3", entry);
+            }
+            _ => {
+                eprintln!("entry {} could not be saved to s3", entry)
+            }
         }
     }
 
     // Terminate the connection to the server.
-    let _ = ftp_stream.quit();
+    ftp_stream.quit().await?;
 
     Ok(())
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(async_main())
 }
